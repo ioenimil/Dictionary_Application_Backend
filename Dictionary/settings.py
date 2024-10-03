f84364d0fd41fc4e04553db558ca6ac19339fee3
Jmint <jeffreymintah737@gmail.com>
2024-10-03 12:40:12 +0000

    added up to the requirements.txt file

--- Dictionary/settings.py
+++ Dictionary/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
