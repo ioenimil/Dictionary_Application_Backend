c849a1876668ea49aaeacd940acff6b0551b2e08
Jmint <jeffreymintah737@gmail.com>
2024-10-04 10:56:50 +0000

    - Work on creating models for the CRUD Operations. - Have the functionality for adding words and listing words in place.

---
+++ dictionary_app/serializers.py
@@ -0,0 +1,22 @@
+# serializers.py for data validation and transformation
+
+from rest_framework import serializers
+from .models import Word
+
+# serializer class for Word model
+class WordSerializer(serializers.ModelSerializer):   
+    class Meta:
+        model = Word
+        fields = (
+            'word', 
+            'definition', 
+            'example',
+            'part_of_speech',
+            'added_on',
+            'updated_on'
+        )
+        
+        read_only_fields = ['added_on', 'updated_on']
+        
+
+    
