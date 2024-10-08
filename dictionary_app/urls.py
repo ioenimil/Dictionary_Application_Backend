7ba86533e2a565411da357949493ef34cac41a6c
olayodeattipoe <mensahattipoesolomonolayode@gmail.com>
2024-10-08 13:02:53 +0000

    added edit word function

--- dictionary_app/urls.py
+++ dictionary_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import WordListView, CreateWordView
+from .views import WordListView, CreateWordView, EditWordView
 
 # URL configuration for Dictionary project.
 urlpatterns = [
     path('add_word/', CreateWordView.as_view(), name='add-word'),
     path('words/', WordListView.as_view(), name='word-list'),
+    path('edit_word/<uuid:id>', EditWordView.as_view(), name='edit-word')
 ]
