631a828f55a5f38f8d16f6daa18c792cabdd017d
Konlan21 <joekonlan16@gmail.com>
2024-10-10 16:26:24 +0000

    Add swagger documentation at /docs

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -16,9 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+# Swagger stuff
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Dictionary Application",
+        default_version='v1',
+        description="API documentation for Dictionary App",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('dictionary_app.urls')),
-    path('api/auth/', include('accounts.urls')), 
+    path('api/auth/', include('accounts.urls')),
+    
+    # swagger paths
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
